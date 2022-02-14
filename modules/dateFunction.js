3d8466e997daa3b5b844a4d40604705cd18e8d35
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-02-14 11:59:16 -0600

    Load dynamically luxon library
    modified:   .gitignore
    new file:   modules/build/luxon.min.js
    modified:   modules/dateFunction.js

--- modules/dateFunction.js
+++ modules/dateFunction.js
@@ -1,11 +1,18 @@
-import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
-import { DateTime as LocalLuxon } from './luxon.min.js';
+let LuxonDateTime;
+let LocalLuxonDateTime;
+
+try {
+  import('../node_modules/luxon/build/es6/luxon.js').then((e) => { LuxonDateTime = e.DateTime; });
+} catch (e) { LuxonDateTime = undefined; }
+// This will not fail as we know there is a local copy of the file
+import('./build/luxon.min.js').then((e) => { LocalLuxonDateTime = e.DateTime; });
 
 function updateDate(element) {
-  const dateTime = DateTime.now();
-  const dateTimeLocal = LocalLuxon.now();
-  const dt = (dateTime !== undefined) ? dateTime : dateTimeLocal;
-  element.textContent = dt.toLocaleString(DateTime.DATETIME_MED);
+  if (LuxonDateTime !== undefined) {
+    element.textContent = LuxonDateTime.now().toLocaleString(LuxonDateTime.DATETIME_MED);
+  } else {
+    element.textContent = LocalLuxonDateTime.now().toLocaleString(LocalLuxonDateTime.DATETIME_MED);
+  }
 }
 
 export default updateDate;
